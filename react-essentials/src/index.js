023355302e62f519b49e267432cc6ccf51265a92
Valentin Ibanescu <ibanescu.valentin@gmail.com>
2021-12-29 09:29:29 +0200

    added some style

--- react-essentials/src/index.js
+++ react-essentials/src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 ReactDOM.render(
-  React.createElement("h1", null, `Hello, World! This is my first React element.`),
+  React.createElement("h1", {style: {color: 'green'}}, `Hello, World! This is my first React element.`),
   document.getElementById('root')
 );
