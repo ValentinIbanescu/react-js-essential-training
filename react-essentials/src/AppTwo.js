99022c6a308df91a3cdfd04ff62be1fe208a4598
Valentin Ibanescu <ibanescu.valentin@gmail.com>
2021-12-30 08:30:30 +0200

    useEffect ok

--- react-essentials/src/AppTwo.js
+++ react-essentials/src/AppTwo.js
@@ -1,14 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function AppTwo() {
   const [emotion, setEmotion] = useState("happy");
+  const [secondary, setSecondary] = useState("tired");
+
+  useEffect(() => {
+    console.log(`It's ${emotion} around here.`)
+  }, [emotion]);
+
+  useEffect(() => {
+    console.log(`It's also ${secondary} here.`)
+  }, [secondary]);
+
   return (
     <>
-      <h1 className="App">Hello, World! The current emotion is {emotion}!</h1>
-      <button onClick={() => setEmotion("happy")}>Happy</button>
+      <h1 className="App">Hello, World! The current emotion is {emotion} and {secondary}!</h1>
+      <button onClick={() => setEmotion("happy")}>Make happy</button>
       <button onClick={() => setEmotion("frustraded")}>Frustrate</button>
       <button onClick={() => setEmotion("motivated")}>Motivate</button>
+      <button onClick={() => setSecondary("crabby")}>Make crabby</button>
     </>
   );
 }
