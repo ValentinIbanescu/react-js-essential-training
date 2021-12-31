2b4f31ff5d6e136316e0698e7a9225c1ad7bf2af
Valentin Ibanescu <ibanescu.valentin@gmail.com>
2021-12-31 08:31:31 +0200

    installed React Router

---
+++ react-essentials/src/pages.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+export function Home() {
+  return (
+    <div>
+      <h1>[Company website]</h1>
+    </div>
+  );
+}
+
+export function About() {
+  return (
+    <div>
+      <h1>[About us]</h1>
+    </div>
+  );
+}
+export function Events() {
+  return (
+    <div>
+      <h1>[Our events]</h1>
+    </div>
+  );
+}
+export function Contact() {
+  return (
+    <div>
+      <h1>[Contact us]</h1>
+    </div>
+  );
+}
