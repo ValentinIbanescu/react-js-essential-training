4bd2ebad86eff60abb05807f2c8b2b34060d7848
Valentin Ibanescu <ibanescu.valentin@gmail.com>
2022-02-03 22:06:40 +0200

    nested components

--- react-essentials/src/App.js
+++ react-essentials/src/App.js
@@ -2,10 +2,28 @@ import React from 'react';
 import './App.css';
 import Header from './Header.js';
 
+function Main() {
+  return (
+    <section>
+      <p>Lorem</p>
+      </section>
+  );
+}
+
+function Footer() {
+  return (
+  <footer>
+    <p>Made with React©</p>
+  </footer>
+  );
+}
+
 function App() {
   return (
     <div className="App">
       <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
